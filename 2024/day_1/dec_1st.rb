42a232ef011960df5151e532dfaa1944be853837
jakecomp <jakobvalen9@gmail.com>
2024-12-01 07:42:02 -0800

    Add day 1 puzzle

---
+++ 2024/day_1/dec_1st.rb
@@ -0,0 +1,51 @@
+def parse_input(path)
+  left_list = []
+  right_list = []
+  File.foreach(path) do |line|
+    content = line.split(" ")
+    left_list << content[0].to_i
+    right_list << content[1].to_i
+  end
+  return left_list, right_list
+end
+
+
+def total_distance_calc()
+  left_list, right_list = parse_input("/mnt/c/Users/jake1/AOC/2024/day_1/input.txt")
+
+  total_distance = 0
+
+  left_list.sort!
+  right_list.sort!
+
+  left_list.each_with_index do |left, index|
+    right = right_list[index]
+    total_distance += (right - left).abs
+  end
+  return total_distance
+end
+
+def similarity_calc()
+  left_list, right_list = parse_input("/mnt/c/Users/jake1/AOC/2024/day_1/input.txt")
+  right_count = {}
+
+  right_list.each do |right|
+    if right_count[right]
+      right_count[right] += 1
+    else
+      right_count[right] = 1
+    end
+  end
+
+  total_similarity = 0
+  left_list.each do |left|
+    if right_count[left]
+      total_similarity += left * right_count[left]
+    end
+  end
+  return total_similarity
+end
+
+puts total_distance_calc()
+puts similarity_calc()
+
